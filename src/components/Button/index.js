80ebb052ff8e944836c92217321acbe51de78a8a
lucascod3 <lukinha_matheus09@hotmail.com>
2024-11-12 22:56:19 -0300

    Terminando o update

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,46 @@
-
 import { ButtonContainer } from './styles';
 
-const Button = ({label, onClick}) => {
-    return (
-      <ButtonContainer onClick={onClick} type="button">
-       {label}
-      </ButtonContainer>
-    );
-  }
-  
-  export default Button;
+/**
+ * Um botão reutilizável que pode ser usado em diferentes partes do aplicativo.
+ * @param {string} label - O texto exibido no botão.
+ * @param {function} onClick - Uma função a ser executada quando o botão for clicado.
+ */
+const Button = ({ label, onClick }) => {
+  return (
+    <ButtonContainer onClick={onClick} type="button" aria-label={label}>
+      {/* Verifique se a prop label existe antes de renderizá-la */}
+      {label && label}
+    </ButtonContainer>
+  );
+};
+
+export default Button;
+
+// codigo alternativo
+
+/*
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ label, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick(label);
+    }
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default Button;
+*/
+
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,16 @@
+import { InputWrapper, Input } from './styles';
 
-import { InputContainer } from './styles';
+/**
+ * Um componente de input reutilizável que pode ser usado em diferentes partes do aplicativo.
+ * @param {string} value - O valor do input.
+ */
+const CustomInput = ({ value }) => {
+  return (
+    <InputWrapper>
+      {/* Desestruture as propriedades e passe apenas as necessárias para o input */}
+      <Input readOnly value={value} />
+    </InputWrapper>
+  );
+};
 
-const Input = ({value}) => {
-    return (
-      <InputContainer>
-       <input disabled value={value}/>
-      </InputContainer>
-    );
-  }
-  
-  export default Input;
+export default CustomInput;
